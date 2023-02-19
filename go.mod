e0821f691c922b8eb93dd28549d57b0e27a7e7fd
Yerzhanov Beksultan <200103513@stu.sdu.edu.kz>
2023-02-19 23:44:34 +0600

    Main.go changed and updated with db, but has some bugs

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module sdustore
 go 1.18
 
 require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- src/Main.go
+++ src/Main.go
@@ -1,55 +1,100 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type User struct {
-	Id                                 int
+	ID                                 int
 	Username, Surname, Password, Email string
 }
 
-var users []User
-
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/register", register)
-	http.HandleFunc("/login", login)
-	http.ListenAndServe(":8080", nil)
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/maindb")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/register", register).Methods("POST")
+	router.HandleFunc("/register", showRegisterForm).Methods("GET")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/login", showLoginForm).Methods("GET")
+	router.HandleFunc("/welcome", welcome)
+	http.ListenAndServe(":8080", router)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("home.html")
+	t, _ := template.ParseFiles("templates/home.html")
+	t.Execute(w, nil)
+}
+
+func showRegisterForm(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/register.html")
 	t.Execute(w, nil)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-		newUser := User{Username: username, Password: password}
-		users = append(users, newUser)
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-	} else {
-		t, _ := template.ParseFiles("register.html")
-		t.Execute(w, nil)
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testdb")
+	if err != nil {
+		panic(err.Error())
 	}
+	defer db.Close()
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	var user User
+	err = db.QueryRow("SELECT id FROM users WHERE username=?", username).Scan(&user.ID)
+	if err != sql.ErrNoRows {
+		t, _ := template.ParseFiles("templates/register.html")
+		t.Execute(w, "Username already taken.")
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", username, password)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func showLoginForm(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/login.html")
+	t.Execute(w, nil)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-		for _, user := range users {
-			if user.Username == username && user.Password == password {
-				http.Redirect(w, r, "/welcome", http.StatusSeeOther)
-				return
-			}
-		}
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-	} else {
-		t, _ := template.ParseFiles("login.html")
-		t.Execute(w, nil)
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testdb")
+	if err != nil {
+		panic(err.Error())
 	}
+	defer db.Close()
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	var user User
+	err = db.QueryRow("SELECT id, username, password FROM users WHERE username=? AND password=?", username, password).Scan(&user.ID, &user.Username, &user.Password)
+	if err != nil {
+		t, _ := template.ParseFiles("templates/login.html")
+		t.Execute(w, "Invalid username or password.")
+		return
+	}
+
+	t, _ := template.ParseFiles("templates/welcome.html")
+	t.Execute(w, user)
+
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/welcome.html")
+	t.Execute(w, nil)
 }
