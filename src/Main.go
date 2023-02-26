cb1585136a22b22b2a991a9b00ff2283d9e7facb
Darkhan Tashim <200103208@stu.sdu.edu.kz>
2023-02-26 22:37:15 +0600

    Added register page

--- src/Main.go
+++ src/Main.go
@@ -4,17 +4,19 @@ import (
 	_ "fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
-	Username string
-	Password string
+	Id                                 int
+	Username, Surname, Password, Email string
 }
 
 var users []User
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/register", registerHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -26,4 +28,36 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t.Execute(w, nil)
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("templates/register.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, nil)
+	} else if r.Method == "POST" {
+		id, _ := strconv.Atoi(r.FormValue("id"))
+		name := r.FormValue("username")
+		surname := r.FormValue("surname")
+		password := r.FormValue("password")
+		email := r.FormValue("email")
+		for _, user := range users {
+			if user.Id == id {
+				http.Error(w, "Username already taken", http.StatusBadRequest)
+				return
+			}
+		}
+
+		users = append(users, User{Id: id, Username: name, Surname: surname, Password: password, Email: email})
+
+		t, err := template.ParseFiles("templates/register-success.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, nil)
+	}
 }
